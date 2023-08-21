5753037872c9fd9683d4a854864761519d2c53e3
alvaradoCarolina <132953183+alvaradoCarolina@users.noreply.github.com>
2023-08-20 22:18:10 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+import {  BrowserRouter,  Routes,  Route } from "react-router-dom";
+import Dashboard from "./layout/Dashboard";
+import Login from "./layout/Login";
+import FormularioLogin from "./paginas/FormularioLogin";
+import FundamentoUseEffect from "./paginas/FundamentoUseEffect";
+import FundamentoUseState from "./paginas/FundamentoUseState";
+import LandingPage from "./paginas/LandingPage";
+import FundamentoUseContext from "./paginas/FundamentoUseContext";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<LandingPage />}>
+        </Route>
+
+        <Route path="/login" element={<Login />}>
+          <Route index element={<FormularioLogin/>}/>
+        </Route>
+
+        <Route path="/fundamentos" element={<Dashboard />}>
+          <Route path="usestate" element={<FundamentoUseState />} />
+          <Route path="useffect" element={<FundamentoUseEffect />} />
+					<Route path="usecontext" element={<FundamentoUseContext />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
